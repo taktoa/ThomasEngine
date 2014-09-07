7b3fb06f7dbbf9ca8ba325054087abd8dda78f09
Remy Goldschmidt <taktoa@gmail.com>
2014-09-07 13:40:42 -0500

    Supertexturing kind of, need to improve muh efficiency

--- main.rkt
+++ main.rkt
@@ -1,6 +1,6 @@
 #lang racket
 (require 
-  "ascii.rkt"
+  "texture.rkt"
   racket/gui
   racket/runtime-path)
 
@@ -11,35 +11,24 @@
        [label "Testing"]
        [style '(no-resize-border)]))
 
-(define tile-w 9)
-(define tile-h 16)
-(define tileset-w 16)
-(define tileset-h 16)
-(define canvas-w 32)
-(define canvas-h 32)
-(define tileset-path (build-path RUNTIME_DIR "tiles.png"))
+(define width 960)
+(define height 540)
 
 (define test-ac
-  (new ascii-canvas% 
+  (new texture-canvas% 
        [parent test-frame]
-       [tiles (tileset tileset-path tile-w tile-h tileset-w tileset-h)]
-       [canvas-width canvas-w]
-       [canvas-height canvas-h]))
+       [texture-path (build-path RUNTIME_DIR "bigtexture.png")]))
 
-(for* ([xi (in-range canvas-w)]
-       [yi (in-range canvas-h)])
-  (send test-ac write 0 xi yi))
+(send test-ac set-position 400 400)
 
 (send test-frame show #t)
 
 (thread
  (lambda ()
-   (let loop ()
-     (send test-ac write 
-           (random 256)
-           (random canvas-w) (random canvas-h))
-     (sleep 0.1)
-     (loop))))
+   (let loop ([x 400] [y 400])
+     (send test-ac set-position x y)
+     (sleep 0.2)
+     (loop (+ x 1) (+ y 1)))))
 
 (thread
  (lambda ()
@@ -47,3 +36,54 @@
      (send test-frame refresh)
      (sleep 0.5)
      (loop))))
+
+
+;#lang racket
+;(require 
+;  "ascii.rkt"
+;  racket/gui
+;  racket/runtime-path)
+;
+;(define-runtime-path RUNTIME_DIR ".")
+;
+;(define test-frame
+;  (new frame%
+;       [label "Testing"]
+;       [style '(no-resize-border)]))
+;
+;(define tile-w 9)
+;(define tile-h 16)
+;(define tileset-w 16)
+;(define tileset-h 16)
+;(define canvas-w 32)
+;(define canvas-h 32)
+;(define tileset-path (build-path RUNTIME_DIR "tiles.png"))
+;
+;(define test-ac
+;  (new ascii-canvas% 
+;       [parent test-frame]
+;       [tiles (tileset tileset-path tile-w tile-h tileset-w tileset-h)]
+;       [canvas-width canvas-w]
+;       [canvas-height canvas-h]))
+;
+;(for* ([xi (in-range canvas-w)]
+;       [yi (in-range canvas-h)])
+;  (send test-ac write 0 xi yi))
+;
+;(send test-frame show #t)
+;
+;(thread
+; (lambda ()
+;   (let loop ()
+;     (send test-ac write 
+;           (random 256)
+;           (random canvas-w) (random canvas-h))
+;     (sleep 0.1)
+;     (loop))))
+;
+;(thread
+; (lambda ()
+;   (let loop ()
+;     (send test-frame refresh)
+;     (sleep 0.5)
+;     (loop))))
---
+++ texture.rkt
@@ -0,0 +1,74 @@
+#lang racket
+(require 
+  "matrix.rkt"
+  racket/draw
+  racket/gui)
+
+(provide
+ (all-defined-out))
+
+(define texture-canvas%
+  (class canvas%
+    (inherit
+      get-width
+      get-height
+      refresh)
+    
+    (init-field
+     parent
+     [texture-path "bigtexture.png"]
+     [width 960]
+     [height 540])
+    
+    (field
+     [texture #f]
+     [offscreen-buffer #f] 
+     [offscreen-buffer-dc #f]
+     [old-position-x 0]
+     [old-position-y 0]
+     [position-x 0]
+     [position-y 0])
+    
+    ; Repaint the canvas
+    (define/private (paint self dc)
+      (unless offscreen-buffer (init-buffer))
+      (draw-texture position-x position-y)
+      (send dc draw-bitmap offscreen-buffer 0 0))
+    
+    (define/private (init-buffer)
+      (set! offscreen-buffer (make-screen-bitmap (get-width) (get-height)))
+      (set! offscreen-buffer-dc (new bitmap-dc% [bitmap offscreen-buffer])))
+    
+    (define/private (draw-texture x y)
+      (send offscreen-buffer-dc draw-bitmap-section
+            texture
+            0
+            0
+            x
+            y
+            (get-width)
+            (get-height)))
+    
+    (define/public (clear) (init-buffer))
+    
+    (define/public (set-position x y)
+      (set! position-x x)
+      (set! position-y y))
+
+    ; Load the glyphs
+    (define texture-file (read-bitmap texture-path 'unknown))
+    (set! texture (make-bitmap (send texture-file get-width) (send texture-file get-height)))
+    (define texture-dc (new bitmap-dc% [bitmap texture]))
+    (send texture-dc draw-bitmap texture-file 0 0)
+    
+    ; Create the canvas
+    (super-new 
+      [parent parent]
+      [paint-callback (λ (c dc) (paint c dc))]
+      [min-width width]
+      [min-height height])
+    
+    ;(send this clear)
+    
+    (send this focus)))
+
