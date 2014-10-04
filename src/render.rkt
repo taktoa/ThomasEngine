1d8c62ea75e713a20dcb8b36f5f2c5323353de8b
Remy Goldschmidt <taktoa@gmail.com>
2014-10-04 15:31:59 -0500

    Things are slightly less shitty

--- src/render.rkt
+++ src/render.rkt
@@ -1,78 +1,96 @@
 #lang racket
-(require racket/draw
-         2htdp/universe)
+(require
+  racket/draw
+  "entity.rkt"
+  2htdp/universe)
 
-(define sprite%
-  (class object%
+(define sprite-entity%
+  (class entity%
     ;; Class fields
+    (inherit
+      update!
+      queue-prop-change!
+      prop-get
+      prop-get-all)
+    
     (init-field
      [sprite #f])
     
     (field
-     [outline-dc #f]
-     [scale 1]
-     [rotation 0]
-     [sprite-w 0]
-     [sprite-h 0])
+     [sprite-dc #f])
     
     ;; Private functions
-    (define/private (gen-outline-bitmap)
-      (make-object bitmap%
-        (* 2 scale sprite-w)
-        (* 2 scale sprite-h)))
-    
-    (define/private (set-sprite-dims)
-      (set! sprite-w (send sprite get-width))
-      (set! sprite-h (send sprite get-height)))
-    
     (define/private (dtr r) (* r (/ (* 2 pi) 360)))
     
+    (define/private (render-sprite)
+      (let ([s (prop-get 'scale)]
+            [r (prop-get 'rotation)]
+            [sw (send sprite get-width)]
+            [sh (send sprite get-height)])
+        (define (gen-blank-bm) (make-bitmap (* 2 s sw) (* 2 s sh) #t))
+        (set! sprite-dc (new bitmap-dc% [bitmap (gen-blank-bm)]))
+        (send sprite-dc set-origin (* s sw) (* s sh))
+        (send sprite-dc set-rotation (dtr r))
+        (send sprite-dc set-scale s s)
+        (send sprite-dc draw-bitmap sprite (* -1/2 sw) (* -1/2 sh))
+        (send sprite-dc set-rotation r)
+        (send sprite-dc get-bitmap)))
+    
     ;; Public functions
-    ; Set the scale
-    (define/public (set-scale! s) (set! scale s))
-    
-    ; Set the rotation
-    (define/public (set-rotation! r) (set! rotation r))
-    
-    ; Render the sprite
     (define/public (render)
-      (set-sprite-dims)
-      (set! outline-dc (new bitmap-dc% [bitmap (gen-outline-bitmap)]))
-      (send outline-dc set-origin
-            (* scale sprite-w)
-            (* scale sprite-h))
-      (send outline-dc set-rotation (dtr rotation))
-      (send outline-dc set-scale scale scale)
-      (send outline-dc draw-bitmap ((λ () sprite)) (* -1/2 sprite-w) (* -1/2 sprite-h))
-      (send outline-dc get-bitmap))
+      (update!)
+      (values
+       (render-sprite)
+       (prop-get 'position-x)
+       (prop-get 'position-y)))
+    
+    (define/public (set-position! x y)
+      (queue-prop-change!
+       (hash 'position-x x 'position-y y)))
+    
+    (define/public (set-rotation! r)
+      (queue-prop-change!
+       (hash 'rotation r)))
+    
+    (define/public (set-scale! s)
+      (queue-prop-change!
+       (hash 'scale s)))
     
     ;; Class initialization
     (super-new)))
 
-(define test-sprite
-  (new sprite%
-       [sprite (read-bitmap "../res/grass.png" 'unknown)]))
+(define (mutate-sprite-entity! se r s x y)
+  (send se set-rotation! r)
+  (send se set-scale! s)
+  (send se set-position! x y))
 
-;(send test-sprite set-scale! 4)
-;
-;(define (animate-example r)
-;  (send test-sprite set-rotation! r)
-;  (send test-sprite render))
-;
-;(animate animate-example)
+(define make-sprite-entity
+  (case-lambda
+    [(bm) (new sprite-entity% [sprite bm])]
+    [(bm r s x y)
+     (define se (make-sprite-entity bm))
+     (mutate-sprite-entity! se r s x y)
+     se]))
 
-(send test-sprite set-rotation! 35)
+(define grass-bm
+  (read-bitmap "../res/grass.png" 'unknown))
+(define bg-bm
+  (read-bitmap "../res/texture.png" 'unknown))
 
-(define big-texture (read-bitmap "../res/texture.png" 'unknown))
+(define my-entities
+  (list (make-sprite-entity grass-bm 35 3 200 200)
+        (make-sprite-entity grass-bm 100 1 100 200)
+        (make-sprite-entity grass-bm 35 2 300 300)
+        (make-sprite-entity grass-bm 45 4 200 400)
+        (make-sprite-entity grass-bm 75 2 400 200)))
 
-(define test-w 500)
-(define test-h 500)
+(define (render-big entities width height background-bm)
+  (define dc (new bitmap-dc% [bitmap (make-object bitmap% width height)]))
+  (send dc draw-bitmap background-bm 0 0)
+  (define (draw-entity entity)
+    (define-values (bm x y) (send entity render))
+    (send dc draw-bitmap bm x y 'xor))
+  (for-each draw-entity entities)
+  (send dc get-bitmap))
 
-(define test-dc
-  (new bitmap-dc% [bitmap (make-object bitmap% test-w test-h)]))
-
-(send test-dc draw-bitmap big-texture 0 0)
-
-(send test-dc draw-bitmap (send test-sprite render) 200 200 'xor)
-
-(send test-dc get-bitmap)
+(render-big my-entities 600 600 bg-bm)
